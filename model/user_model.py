3ee97ce146727e2ffd59f9f652701e9124d58f08
panchanan.turuk <panchanan.turuk@P3001316>
2024-09-26 11:29:46 +0530

    POST-Method

--- model/user_model.py
+++ model/user_model.py
@@ -5,10 +5,12 @@ class user_model():
         # Connection establishment code
         try:
             self.con = mysql.connector.connect(host="localhost", user="root", password="monumartinez", database="flask_api")
+            self.con.autocommit=True
             self.cur = self.con.cursor(dictionary=True)
             print("Connection Sucessfull")
         except:
             print("Database connection failure")
+
     def user_getall_model(self):
         # Business Logic
         self.cur.execute("SELECT * FROM users")
@@ -17,3 +19,9 @@ class user_model():
             return json.dumps(result)
         else:
             return "No Data Found"
+        
+    def user_addone_model(self, data):
+        # Business Logic
+        # self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}') ")
+        self.cur.execute(f"INSERT INTO users(name, email, phone, role, password) VALUES('{data['name']}', '{data['email']}', '{data['phone']}', '{data['role']}', '{data['password']}') ")
+        return "Profile Created Sucessfully.........."
